dd5216a035f5e2770e0259907cffe8d9502c90a9
Andrew <andrewmarine7882@gmail.com>
2019-12-27 11:04:13 -0800

    initial commit

---
+++ pkg/tracing/tracer.go
@@ -0,0 +1,82 @@
+package tracing
+
+import (
+	"io"
+
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaeger_zap "github.com/uber/jaeger-client-go/log/zap"
+	"github.com/uber/jaeger-lib/metrics/prometheus"
+	"go.uber.org/zap"
+)
+
+// Tracing contains all of the tracing object and methods needed to interact with
+// the constructed tracing interface
+type Tracing struct {
+	Tracer        opentracing.Tracer
+	reporter      jaeger.Reporter
+	tracingCloser io.Closer
+}
+
+// CloseTracing closes the tracing and reporting objects
+func (t *Tracing) CloseTracing() error {
+	t.reporter.Close()
+	return t.tracingCloser.Close()
+}
+
+// NewTracer configures a jaeger tracing setup and returns the the configured tracer and reporter for use
+//
+// Arguments:
+// serviceName: The name of the service (app) in tracing messages
+// transportDestination: The jaeger server where we are sending the remote reporting to if enabled. <host>:<port> ie "localhost:3001"
+// reportRemote: True enables remote reporting
+// logger accepts: Zap logger to use for logging tracing reporting
+// metricTags: Key value tags appended to the tracing logs
+// sample: True will only report on a sampling of requests. Reporting on all requests on prod would be untenable
+//
+func NewTracer(serviceName string, transportDestination string, reportRemote bool, logger *zap.Logger, metricTags map[string]string, sample bool) (*Tracing, error) {
+	t := Tracing{}
+
+	// Adapt the zap logger to work with jaeger
+	adaptedLogger := jaeger_zap.NewLogger(logger)
+
+	// create a metrics object
+	factory := prometheus.New()
+	metrics := jaeger.NewMetrics(factory, metricTags)
+
+	if reportRemote {
+		// If we want to report to a remote tracing analytics server
+		// create the connection here
+		transport, err := jaeger.NewUDPTransport(transportDestination, 0)
+		if err != nil {
+			return &t, err
+		}
+
+		// create composite logger to log to the logger and report to the
+		// remote server
+		t.reporter = jaeger.NewCompositeReporter(
+			jaeger.NewLoggingReporter(adaptedLogger),
+			jaeger.NewRemoteReporter(transport,
+				jaeger.ReporterOptions.Metrics(metrics),
+				jaeger.ReporterOptions.Logger(adaptedLogger),
+			),
+		)
+	} else {
+		// Simple, logging only reporter
+		t.reporter = jaeger.NewLoggingReporter(adaptedLogger)
+	}
+
+	// create a sampler for the spans so that we don't report every single span which would be untenable
+	sampler := jaeger.NewConstSampler(sample)
+
+	// now make the tracer
+	t.Tracer, t.tracingCloser = jaeger.NewTracer(serviceName,
+		sampler,
+		t.reporter,
+		jaeger.TracerOptions.Metrics(metrics),
+	)
+
+	opentracing.SetGlobalTracer(t.Tracer)
+
+	return &t, nil
+}
