2d4510a4211d7ce19243199c44907fd88b341701
Andrew <andrewmarine7882@gmail.com>
2019-12-28 01:00:42 -0800

    add an example

---
+++ examples/tracing/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+
+	"github.com/caring/go-packages/pkg/tracing"
+	"github.com/opentracing/opentracing-go"
+	"go.uber.org/zap"
+)
+
+func main() {
+	t, err := tracing.NewTracer(
+		"my-service",
+		"hostname:6451",
+		false,
+		zap.L(),
+		map[string]string{
+			"tag": "value",
+		},
+		1.0,
+		1.0,
+	)
+	defer tracing.CloseTracing()
+
+	if err != nil {
+		log.Fatal("Error establishing tracing")
+	}
+
+	opentracing.SetGlobalTracer(t.Tracer)
+}
