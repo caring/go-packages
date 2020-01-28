ee232e760786ebc9f0fd8bcfd344264c4f4c9e16
Andrew <andrewmarine7882@gmail.com>
2020-01-28 15:52:10 -0800

    Add more examples

--- examples/logging/main.go
+++ examples/logging/main.go
@@ -3,22 +3,38 @@ package main
 import "github.com/caring/go-packages/pkg/logging"
 
 func main() {
-	l, err := logging.InitLogging(false, "logger-1", "stream-1", "call-scoring", "aws-key", "aws-secret", "aws-region", true, false)
+	parent, err := logging.InitLogging(false, "logger-1", "stream-1", "call-scoring", "aws-key", "aws-secret", "aws-region", true, false)
 
 	if err != nil {
 		panic(err)
 	}
 
-	l.NewChild("", "endpoint2", 3, 3, 2, 1, false, nil)
+	child := parent.NewChild("", "endpoint2", 3, 3, 2, 1, false, []logging.Field{logging.NewStringField("child", "this wont be in the parent")})
 
-	l.Warn("sample message", []logging.Field{logging.NewInt64Field("fieldA", 3)}...)
+	child.Warn("sample message", []logging.Field{logging.NewInt64Field("fieldA", 3)}...)
 
-	l.AppendAdditionalData([]logging.Field{logging.NewBoolField("fieldB", true)})
+	child.AppendAdditionalData([]logging.Field{logging.NewBoolField("fieldB", true)})
 
-	l.Warn("here's another waring")
+	parent.Warn("here's another waring, see no child field!")
 
-	l.SetIsReportable(true)
-	l.SetAdditionalData(nil)
+	child.SetIsReportable(true)
+	child.SetServiceID("some service")
 
-	l.Warn("final warning")
+	child.Warn("I'm the child, final warning... see my fields have changed!", logging.NewFloat64sField("floats", []float64{1.0, 2.0, 3.0}))
+
+	m := map[string]interface{}{
+		"key1": 1.0,
+		"key2": true,
+		"key3": "string",
+		"key4": map[string]interface{}{
+			"key5": 459876,
+		},
+	}
+	obj := map[string]interface{}{
+		"key1": 2134,
+		"key2": true,
+	}
+	a := []map[string]interface{}{obj, obj}
+
+	parent.Warn("Look how any field can marshal complex objects, this is expensive!", logging.NewAnyField("map", m), logging.NewAnyField("array", a))
 }
