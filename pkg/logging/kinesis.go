86af9ba07e362357f721fa202e2e8c261092e41c
Andrew <andrewmarine7882@gmail.com>
2020-05-07 14:54:02 -0700

    strip out kinesis AWS config, level config, and debugging read code that was slowing kinesis down

--- pkg/logging/kinesis.go
+++ pkg/logging/kinesis.go
@@ -9,60 +9,35 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-// AllLevels Supported log levels
-var AllLevels = []zapcore.Level{
-	zapcore.DebugLevel,
-	zapcore.InfoLevel,
-	zapcore.WarnLevel,
-	zapcore.ErrorLevel,
-	zapcore.FatalLevel,
-	zapcore.PanicLevel,
-}
-
 // KinesisHook provides the details to hook into the Zap logger
 type KinesisHook struct {
-	svc            *kinesis.Kinesis
-	Async          bool
-	AcceptedLevels []zapcore.Level
-	streamName     string
-	m              sync.Mutex
-	isProd         bool
-	serviceID      string
+	svc             *kinesis.Kinesis
+	partitionKey    string
+	writeableLevels []zapcore.Level
+	streamName      string
+	m               sync.Mutex
+	serviceID       string
 }
 
 // newKinesisHook creates a KinesisHook struct to to use in the zap log.
 // Tries to find the existing aws Kinesis stream.
 // Creates stream when doesn't exist.
 // Returns a pointer with a implemented KinesisHook.
-func newKinesisHook(streamName string, cfg *aws.Config, isProd, isAsync bool) (*KinesisHook, error) {
-	s := session.New(cfg)
+func newKinesisHook(streamName string, partitionKey string) (*KinesisHook, error) {
+	s := session.New()
 	kc := kinesis.New(s)
 
 	_, err := kc.DescribeStream(&kinesis.DescribeStreamInput{StreamName: aws.String(streamName)})
 
 	// Create stream if doesn't exist
 	if err != nil {
-		_, err := kc.CreateStream(&kinesis.CreateStreamInput{
-			ShardCount: aws.Int64(1),
-			StreamName: aws.String(streamName),
-		})
-
-		if err != nil {
-			return nil, err
-		}
-
-		if err := kc.WaitUntilStreamExists(&kinesis.DescribeStreamInput{StreamName: aws.String(streamName)}); err != nil {
-			return nil, err
-		}
+		return nil, err
 	}
 
 	ks := &KinesisHook{
-		streamName:     streamName,
-		svc:            kc,
-		AcceptedLevels: AllLevels,
-		m:              sync.Mutex{},
-		isProd:         isProd,
-		Async:          isAsync,
+		streamName: streamName,
+		svc:        kc,
+		m:          sync.Mutex{},
 	}
 
 	return ks, nil
@@ -71,76 +46,23 @@ func newKinesisHook(streamName string, cfg *aws.Config, isProd, isAsync bool) (*
 // getHook inserts the function to use when zap creates a log entry.
 func (ch *KinesisHook) getHook() (func(zapcore.Entry) error, error) {
 	kWriter := func(e zapcore.Entry) error {
-		if !ch.isAcceptedLevel(e.Level) {
-			return nil
-		}
-
 		writer := func() error {
-			partKey := "logging-1"
-
-			putOutput, err := ch.svc.PutRecord(&kinesis.PutRecordInput{
+			_, err := ch.svc.PutRecord(&kinesis.PutRecordInput{
 				Data:         []byte(e.Message),
 				StreamName:   aws.String(ch.streamName),
-				PartitionKey: &partKey,
+				PartitionKey: &ch.partitionKey,
 			})
 
 			if err != nil {
 				return err
-			}
-
-			// retrieve iterator
-			iteratorOutput, err := ch.svc.GetShardIterator(&kinesis.GetShardIteratorInput{
-				// Shard Id is provided when making put record(s) request.
-				ShardId:           putOutput.ShardId,
-				ShardIteratorType: aws.String("TRIM_HORIZON"),
-				// ShardIteratorType: aws.String("AT_SEQUENCE_NUMBER"),
-				// ShardIteratorType: aws.String("LATEST"),
-				StreamName: aws.String(ch.streamName),
-			})
-			if err != nil {
-				return err
-			}
-
-			// get records use shard iterator for making request
-			records, err := ch.svc.GetRecords(&kinesis.GetRecordsInput{
-				ShardIterator: iteratorOutput.ShardIterator,
-			})
-			if err != nil {
-				return err
-			}
-
-			if !ch.isProd && len(records.Records) > 0 {
-				lastRecord := len(records.Records) - 1
-				println(records.Records[lastRecord].String())
 			}
 
 			return err
 		}
 
-		if ch.Async {
-			go writer()
-
-			return nil
-		}
-		return writer()
+		go writer()
+		return nil
 	}
 
 	return kWriter, nil
-}
-
-// Levels sets which levels to sent to kinesis
-func (ch *KinesisHook) levels() []zapcore.Level {
-	if ch.AcceptedLevels == nil {
-		return AllLevels
-	}
-	return ch.AcceptedLevels
-}
-
-func (ch *KinesisHook) isAcceptedLevel(level zapcore.Level) bool {
-	for _, lv := range ch.levels() {
-		if lv == level {
-			return true
-		}
-	}
-	return false
 }
