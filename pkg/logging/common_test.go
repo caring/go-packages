ce9765728f2e1c5325031145c6863e81080d64d4
Andrew <andrewmarine7882@gmail.com>
2020-05-26 15:06:43 -0700

    implement some testing utils for observing log out put and running a test logger

---
+++ pkg/logging/common_test.go
@@ -0,0 +1,46 @@
+package logging
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.uber.org/zap/zaptest/observer"
+)
+
+// A utility method ported from Uber's zap lib to aid in testing log outputs
+func withLogger(c *Config, f func(*Logger, *observer.ObservedLogs)) {
+	l := zapcore.InfoLevel
+	err := l.UnmarshalText([]byte(c.LogLevel))
+	if err != nil {
+		panic(err)
+	}
+	fac, logs := observer.New(l)
+	zapL := zap.New(fac)
+
+	log, err := NewLogger(c)
+	if err != nil {
+		panic(err)
+	}
+	log.internalLogger = zapL
+	f(log, logs)
+}
+
+// A testing util that creates an array of zap fields that would be the expected
+// log output for a given config and any additional fields provided
+func commonFields(c *Config, i *InternalFields, additional ...zap.Field) []zap.Field {
+	fields := make([]zap.Field, 7)
+
+	var b bool
+	if i.IsReportable != nil {
+		b = *i.IsReportable
+	}
+
+	fields[0] = String("service", c.ServiceName).field
+	fields[1] = String("endpoint", i.Endpoint).field
+	fields[2] = Bool("isReportable", b).field
+	fields[3] = String("traceabilityID", i.TraceabilityID).field
+	fields[4] = String("correlationID", i.CorrelationID).field
+	fields[5] = String("userID", i.UserID).field
+	fields[6] = String("clientID", i.ClientID).field
+
+	return append(fields, additional...)
+}
