86b584e912e045eb09ace1dd169ce3e10fe69304
Andrew <andrewmarine7882@gmail.com>
2020-05-27 04:07:42 -0700

    port context helpers from the zap lib

--- pkg/logging/context/context.go
+++ pkg/logging/context/context.go
@@ -1,13 +1,50 @@
-package ctx
+package logctx
 
 import (
 	"context"
-	"errors"
 
 	"github.com/caring/go-packages/pkg/logging"
+	grpc_ctxtags "github.com/grpc-ecosystem/go-grpc-middleware/tags"
 )
 
-// Extract gets a Logger instance from a context.Context
-func Extract(ctx context.Context) (*logging.Logger, error) {
-	return logging.NewNopLogger(), errors.New("Not implemented")
+type ctxMarker struct{}
+
+type ctxLogger struct {
+	logger *logging.Logger
+}
+
+var (
+	ctxKey     = &ctxMarker{}
+	nullLogger = logging.NewNopLogger()
+)
+
+// Extract gets a Logger instance from a context.Context, it always returns a logger
+// populated with the latest gRPC tags
+func Extract(ctx context.Context) *logging.Logger {
+	l, ok := ctx.Value(ctxKey).(*ctxLogger)
+	if !ok || l == nil {
+		return nullLogger
+	}
+
+	fields := TagsToFields(ctx)
+	return l.logger.With(nil, fields...)
+}
+
+// TagsToFields transforms the gRPC Tags on the supplied context into structured fields.
+func TagsToFields(ctx context.Context) []logging.Field {
+	fields := []logging.Field{}
+	tags := grpc_ctxtags.Extract(ctx)
+	for k, v := range tags.Values() {
+		fields = append(fields, logging.Any(k, v))
+	}
+	return fields
+}
+
+// ToContext adds the Logger to the context for extraction later.
+// Returning the new context that has been created.
+func ToContext(ctx context.Context, logger *logging.Logger) context.Context {
+	l := &ctxLogger{
+		logger: logger,
+	}
+	return context.WithValue(ctx, ctxKey, l)
 }
