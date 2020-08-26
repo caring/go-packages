3a77f5819bed823dcbcc599bf556f993faf1deca
ShyhShyan Hsu <shsu@caring.com>
2020-08-26 11:36:24 -0600

    PLFM-169 - Create Go library for email SNS : remove const EmailTopic

--- pkg/messaging/sns.go
+++ pkg/messaging/sns.go
@@ -5,11 +5,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
-	"strings"
-)
-
-const (
-	EmailTopic = "EmailTopic"
 )
 
 // NewSNS initializes a new AWS SNS client
@@ -40,19 +35,5 @@ func NewSNS(config *Config) (*sns.SNS, string, error) {
 		l.Fatal("Failed to establish connection to SNS")
 		return nil, "", err
 	}
-
-	result, err := client.ListTopics(nil)
-	if err != nil {
-		l.Fatal("Failed to list SNS topics:" + err.Error())
-		return nil, "", err
-	}
-
-	topicArn := ""
-	for _, t := range result.Topics {
-		if strings.Contains(*t.TopicArn, EmailTopic) {
-			topicArn = *t.TopicArn
-			break
-		}
-	}
-	return client, topicArn, nil
+	return client, c.TopicArn, nil
 }
