366d57436a9007472643b0c8cf2b163d26836ae0
ShyhShyan Hsu <shsu@caring.com>
2020-12-02 09:49:00 -0700

    Use RoleArn or AccessKeyID plus SecretAccessKey to access AWS SQS and SNS

--- pkg/messaging/sns.go
+++ pkg/messaging/sns.go
@@ -2,6 +2,7 @@ package messaging
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
@@ -14,14 +15,32 @@ func NewSNS(config *Config) (*sns.SNS, error) {
 		return nil, err
 	}
 
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Region: aws.String(c.AWSRegion),
-		},
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	creds := stscreds.NewCredentials(sess, c.RoleArn)
-	client := sns.New(sess, &aws.Config{Credentials: creds})
+	var client *sns.SNS
+	if c.RoleArn != "" {
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				Region: aws.String(c.AWSRegion),
+			},
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		creds := stscreds.NewCredentials(sess, c.RoleArn)
+		client = sns.New(sess, &aws.Config{Credentials: creds})
+	} else if c.AccessKeyID != "" && c.SecretAccessKey != "" {
+		credVal := credentials.Value{
+			AccessKeyID:     c.AccessKeyID,
+			SecretAccessKey: c.SecretAccessKey,
+		}
+		cred := credentials.NewStaticCredentialsFromCreds(credVal)
+
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				Credentials: cred,
+				Region:      aws.String(c.AWSRegion),
+			},
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		client = sns.New(sess)
+	}
 	if client == nil {
 		return nil, err
 	}
--- pkg/messaging/sqs.go
+++ pkg/messaging/sqs.go
@@ -2,6 +2,7 @@ package messaging
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
@@ -14,14 +15,31 @@ func NewSQS(config *Config) (*sqs.SQS, error) {
 		return nil, err
 	}
 
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		Config: aws.Config{
-			Region: aws.String(c.AWSRegion),
-		},
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	creds := stscreds.NewCredentials(sess, c.RoleArn)
-	client := sqs.New(sess, &aws.Config{Credentials: creds})
+	var client *sqs.SQS
+	if c.RoleArn != "" {
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				Region: aws.String(c.AWSRegion),
+			},
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		creds := stscreds.NewCredentials(sess, c.RoleArn)
+		client = sqs.New(sess, &aws.Config{Credentials: creds})
+	} else if c.AccessKeyID != "" && c.SecretAccessKey != "" {
+		credVal := credentials.Value{
+			AccessKeyID:     c.AccessKeyID,
+			SecretAccessKey: c.SecretAccessKey,
+		}
+		cred := credentials.NewStaticCredentialsFromCreds(credVal)
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			Config: aws.Config{
+				Credentials: cred,
+				Region:      aws.String(c.AWSRegion),
+			},
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+		client = sqs.New(sess)
+	}
 	if client == nil {
 		return nil, err
 	}
