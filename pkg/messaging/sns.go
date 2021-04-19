eb9cfa1c491d11364aa165c00461a5a06bc604d6
ShyhShyan Hsu <shsu@caring.com>
2021-04-19 15:31:39 -0600

    EMP-64 Go SDK should have method to initialize all the SNS connections

--- pkg/messaging/sns.go
+++ pkg/messaging/sns.go
@@ -6,47 +6,47 @@ import (
 	_ "github.com/aws/aws-sdk-go/aws/credentials/stscreds"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sns"
+	"strings"
 )
 
-// NewSNS initializes a new AWS SNS client
-func NewSNS(config *Config) (*sns.SNS, error) {
+// NewSNS initializes a new AWS SNS client and returns topicArn - default for EmailTopic topic
+func NewSNS(config *Config, params ...string) (*sns.SNS, string, error) {
 	c, err := mergeAndPopulateConfig(config)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 
 	awscfg := &aws.Config{
 		Region:                        aws.String(c.AWSRegion),
 		CredentialsChainVerboseErrors: aws.Bool(true),
 	}
-	// if c.RoleArn != "" {
-	// 	creds := stscreds.NewCredentials(sess, c.RoleArn)
-	// 	awscfg.Credentials = creds
-	// } else if c.AccessKeyID != "" && c.SecretAccessKey != "" {
-	// 	credVal := credentials.Value{
-	// 		AccessKeyID:     c.AccessKeyID,
-	// 		SecretAccessKey: c.SecretAccessKey,
-	// 	}
-	// 	cred := credentials.NewStaticCredentialsFromCreds(credVal)
-	// 	awscfg.Credentials = cred
-	// 	sess = session.Must(session.NewSessionWithOptions(session.Options{
-	// 		Config: aws.Config{
-	// 			Credentials: cred,
-	// 			Region:      aws.String(c.AWSRegion),
-	// 		},
-	// 		SharedConfigState: session.SharedConfigEnable,
-	// 	}))
-	// }
 
 	sess, err := session.NewSession(awscfg)
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 
 	var client *sns.SNS
 	client = sns.New(sess)
 	if client == nil {
-		return nil, err
+		return nil, "", err
 	}
-	return client, nil
+
+	topic := "EmailTopic"
+	if len(params) > 0 {
+		topic = params[0]
+	}
+	topics, err := client.ListTopics(nil)
+	if err != nil {
+		return nil, "", err
+	}
+
+	topicArn := ""
+	for _, t := range topics.Topics {
+		if strings.Contains(*t.TopicArn, topic) {
+			topicArn = *t.TopicArn
+			break
+		}
+	}
+	return client, topicArn, nil
 }
