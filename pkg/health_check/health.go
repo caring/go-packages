8be085783d5492673ddc9ba1b1d559a8932bd082
Clay Raynor <cd.raynor@gmail.com>
2021-06-01 14:02:54 -0400

    Add NewEndpoint() function

--- pkg/health_check/health.go
+++ pkg/health_check/health.go
@@ -1,1 +1,28 @@
 package health_check
+
+type Endpoint struct {
+	Branch 		string
+	SHA1   		string
+	Tag    		string
+	GoVersion 	string
+}
+
+// NewEndpoint returns a initialized Endpoint struct
+func NewEndpoint(branch, sha1, tag, goVersion *string) *Endpoint {
+	var Tag string
+
+	if  len(*tag) < 1 {
+		Tag = *tag
+	} else {
+		Tag = "N/A"
+	}
+
+
+	return &Endpoint{
+		Branch: *branch,
+		SHA1:   *sha1,
+		Tag: Tag,
+		GoVersion: *goVersion,
+	}
+}
+
